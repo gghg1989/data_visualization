ce0d38c4c4957fe4c95d978f868ec17ac17fd2ca
lijw-Broccoli <jingweilifiu@gmail.com>
2019-03-23 17:30:22 -0400

    add animation data

--- Source/App.js
+++ Source/App.js
@@ -335,13 +335,33 @@ WebGLGlobeDataSource.prototype._setLoading = function(isLoading) {
 
 //Create a Viewer instances and add the DataSource.
 var viewer = new Cesium.Viewer('cesiumContainer', {
-    animation : true,
+    //animation : true,
+    shouldAnimate : true,
     timeline : true,
     baseLayerPicker : false
 });
-viewer.clock.shouldAnimate = true;
+//viewer.clock.shouldAnimate = true;
 //viewer.dataSources.add(dataSource);
+function dataCallback(interval, index) {
+    var time;
+    if (index === 0) { // leading
+        time = Cesium.JulianDate.toIso8601(interval.stop);
+    } else {
+        time = Cesium.JulianDate.toIso8601(interval.start);
+    }
 
+    return {
+        Time: time
+    };
+}
+
+var times = Cesium.TimeIntervalCollection.fromIso8601({
+    iso8601: '2015-07-30/2017-06-16/P1D',
+    leadingInterval: true,
+    trailingInterval: true,
+    isStopIncluded: false, // We want stop time to be part of the trailing interval
+    dataCallback: dataCallback
+});
 
 //Get scene of current viewer
 var scene = viewer.scene;
@@ -397,32 +417,39 @@ function setupLayers() {
     
     addBaseLayerOption(
         'weather',
-        new Cesium.BingMapsImageryProvider({
-            url : 'https://dev.virtualearth.net',
-            mapStyle: Cesium.BingMapsStyle.ROAD
+        new Cesium.createWorldImagery({
+            //style: Cesium.IonWorldImageryStyle.AERIAL_WITH_LABELS
         }));
     addBaseLayerOption(
         'temperature',
-        new Cesium.ArcGisMapServerImageryProvider({
-            url : 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer'
+        new Cesium.createWorldImagery({
+            //style: Cesium.IonWorldImageryStyle.AERIAL_WITH_LABELS
         }));
     addBaseLayerOption(
         'pressure',
-        new Cesium.createOpenStreetMapImageryProvider());
+        new Cesium.createWorldImagery({
+            //style: Cesium.IonWorldImageryStyle.AERIAL_WITH_LABELS
+        }));
     addBaseLayerOption(
         'hurricanes',
-        new Cesium.createOpenStreetMapImageryProvider({
-            url : 'https://otile1-s.mqcdn.com/tiles/1.0.0/osm/'
-        }));
-    addBaseLayerOption(
-        'humidity',
-        new Cesium.createOpenStreetMapImageryProvider({
-            url : 'https://stamen-tiles.a.ssl.fastly.net/watercolor/',
-            fileExtension: 'jpg',
-            credit: 'Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.'
+         new Cesium.WebMapTileServiceImageryProvider({
+            url : 'https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/AMSR2_Snow_Water_Equivalent/default/{Time}/{TileMatrixSet}/{TileMatrix}/{TileRow}/{TileCol}.png',
+            layer : 'AMSR2_Snow_Water_Equivalent',
+            style : 'default',
+            tileMatrixSetID : '2km',
+            maximumLevel : 5,
+            format : 'image/png',
+            clock: viewer.clock,
+            times: times,
+            credit : 'NASA Global Imagery Browse Services for EOSDIS'
         }));
 
-    
+    addBaseLayerOption(
+        'humidity',
+        new Cesium.createWorldImagery({
+            //style: Cesium.IonWorldImageryStyle.AERIAL_WITH_LABELS
+        }));
+
     addAdditionalLayerOption(
         'United States Weather Radar',
         new Cesium.WebMapServiceImageryProvider({
@@ -449,10 +476,14 @@ function addBaseLayerOption(name, imageryProvider,dataSource) {
     if (typeof imageryProvider === 'undefined') {
         layer = imageryLayers.get(0);
         viewModel.selectedLayer = layer;
-    } else {
+    }else {
+        if(imageryProvider._layer==="AMSR2_Snow_Water_Equivalent"){
+            setAnimationData(imageryProvider);
+        }
         layer = new Cesium.ImageryLayer(imageryProvider);
+                
+        
     }
-
     layer.name = name;
     baseLayers.push(layer);
 
@@ -530,13 +561,31 @@ function changeDataType(layerName){
 
     }else if (layerName == "humidity") {
         viewer.dataSources.remove(dataSource);
-        getResourceData('../Source/SampleData/population2017.json');
+        getResourceData('../Source/SampleData/ .json');
         viewer.dataSources.add(dataSource);
 
     }
     
 }
 
+function setAnimationData(layerObject){
+    var imageryLayers = new Cesium.ImageryLayer(layerObject);
+    layerObject.readyPromise
+        .then(function() {
+            var start = Cesium.JulianDate.fromIso8601('2015-07-30');
+            var stop = Cesium.JulianDate.fromIso8601('2017-06-17');
+
+            viewer.timeline.zoomTo(start, stop);
+
+            var clock = viewer.clock;
+            clock.startTime = start;
+            clock.stopTime = stop;
+            clock.currentTime = start;
+            clock.clockRange = Cesium.ClockRange.LOOP_STOP;
+            clock.multiplier = 86400;
+        });
+
+}
 
 
 
